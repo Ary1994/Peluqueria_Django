49a859570ce0140c9615e9fd7dad578ec9a0c70e
Ary <ariadna.des.garcia@gmail.com>
2023-06-22 17:14:12 -0300

    proyecto sin el login

---
+++ ProyectoWeb/autenticacion/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.views.generic import View
+from django.contrib.auth.forms import UserCreationForm
+# Create your views here.
+class VRegistro(View):
+    def get(self,request):
+       pass
+    def post(self,request):
+        pass
